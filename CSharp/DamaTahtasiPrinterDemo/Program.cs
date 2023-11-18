b1f79c5d38d0b165a6e1e5ca43b8bdf542e09dbc
Onur Melikoğlu <onurmelikoglu95@gmail.com>
2023-11-18 14:50:17 +0300

    checkerboard pattern added

---
+++ CSharp/DamaTahtasiPrinterDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CheckerboardPatternDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    // Tek ve çift durumuna göre bir yıldız ve bir boşluk çizer
+                    if ((i + j) % 2 == 0) Console.Write("*");
+                    else Console.Write(" "); 
+                }
+                // Yeni satıra geçer
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
