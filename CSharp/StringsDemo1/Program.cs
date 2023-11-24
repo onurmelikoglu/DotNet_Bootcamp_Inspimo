30074eaf69ab7741c4db6b6fda035b906edd31f7
Onur Melikoğlu <onurmelikoglu95@gmail.com>
2023-11-24 22:14:54 +0300

    Update Program.cs

--- CSharp/StringsDemo1/Program.cs
+++ CSharp/StringsDemo1/Program.cs
@@ -4,19 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Cümle(Ç: çıkış)"); 
-            string cumle = Console.ReadLine(); 
-            int index = 0; 
-            while (cumle.ToLower() != "ç") {
-                while (index < cumle.Length)
+            Console.Write("Cümle(Ç: çıkış)");
+            string cumle = Console.ReadLine();
+            int index = 0;
+            int wordCount = 0;
+            while( cumle.ToLower() != "ç")
+            {
+                for(int i = 0; i < cumle.Length; i++)
                 {
-                    int space = cumle.IndexOf(' ', index); 
-                    if (space == -1) { Console.WriteLine(cumle.Substring(index)); break; } 
-                    else { Console.WriteLine(cumle.Substring(index, space - index)); index = space + 1; } 
-                } 
+                    int space = cumle.IndexOf(' ', i);
+                    if (space == -1) { Console.WriteLine(cumle.Substring(i)); break; }
+                    else { Console.WriteLine(cumle.Substring(i, space - i)); i = space + 1; wordCount++; }
+                }
+                Console.WriteLine("Kelime Sayısı: " + ( wordCount + 1 ));
                 Console.Write("Cümle (Ç: çıkış)");
                 cumle = Console.ReadLine();
-                index = 0; 
+                wordCount = 0;
             }
         }
     }
