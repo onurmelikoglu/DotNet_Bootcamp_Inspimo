12993c616ba1903ba123493f807ab9faf6eda4bd
Onur Melikoğlu <onurmelikoglu95@gmail.com>
2023-12-02 13:08:11 +0300

    math and sorting added

---
+++ CSharp/BubbleSort/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] test1 = { 4, 2, 3, 5, 1 };
+            decimal[] test2 = { 4, 4, 3, 3, 1 };
+            decimal[] test3 = { 6, 3, 2, 5, 1, 9, 8, 4, 7 };
+            decimal[] test4 = { 7, 9, 8, 5, 5, 3, 2, 4, 6 };
+            Yazdir(BubbleSort(test1));
+            Yazdir(BubbleSort(test2));
+            Yazdir(BubbleSort(test3));
+            Yazdir(BubbleSort(test4));
+            Array.Sort(test1);
+            Yazdir(test1);
+        }
+
+        static void Yazdir(decimal[] veriKumesi) 
+        {
+            string sonuc = "";
+            foreach(var veri in veriKumesi)
+            {
+                sonuc += veri + ", ";
+            }
+            sonuc = sonuc.TrimEnd(", ".ToCharArray());
+            Console.WriteLine(sonuc);
+        }
+
+        static decimal[] BubbleSort(decimal[] veriKumesi)
+        {
+            decimal[] siralanmisVeriKumesi = new decimal[veriKumesi.Length];
+            // 1. yöntem
+            for(int i = 0; i < veriKumesi.Length; i++)
+            {
+                siralanmisVeriKumesi[i] = veriKumesi[i];
+            }
+            // 2. yöntem
+            siralanmisVeriKumesi = veriKumesi.ToArray();
+
+            decimal gecici;
+            bool degistirildiMi = true;
+            for(int i = 0; i < siralanmisVeriKumesi.Length; i++)
+            {
+                degistirildiMi = false;
+                for(int j = 0; j < siralanmisVeriKumesi.Length - 1; j++)
+                {
+                    if(siralanmisVeriKumesi[j] > siralanmisVeriKumesi[j + 1])
+                    {
+                        gecici = siralanmisVeriKumesi[j];
+                        siralanmisVeriKumesi[j] = siralanmisVeriKumesi[j + 1];
+                        siralanmisVeriKumesi[j + 1] = gecici;
+                        degistirildiMi = true;
+                    } 
+                }
+            }
+
+            return siralanmisVeriKumesi;
+        }
+
+    }
+}
---
+++ CSharp/MathClass/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Globalization;
+
+namespace MathClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 20;
+            int mutlakDeger = Math.Abs(sayi1 - sayi2);
+            Console.WriteLine("sayi1 - sayi2 = " + (sayi1 - sayi2) + ", mutlak deger: " + mutlakDeger);
+
+            Console.WriteLine($"Büyük: {Math.Max(sayi1,sayi2)}, Küçük: {Math.Min(sayi1,sayi2)}");
+
+            int us = 2;
+            Console.WriteLine($"sayi1 karesi: {sayi1 * sayi1}, {Math.Pow(sayi1,us)}");
+
+            int sayi4 = 16;
+            Console.WriteLine("sayi4 kare kökü: " + Math.Sqrt(sayi4)); // 4
+
+            Console.WriteLine("8 küp kökü: " + Math.Pow(8, 1.0 / 3.0) );
+
+            double sayi5 = 12.345;
+            double sayi6 = 98.765;
+
+            Console.WriteLine("taban: " + Math.Floor(sayi5) ); // 12
+            Console.WriteLine("taban: " + Math.Ceiling(sayi5) ); // 13
+
+            Console.WriteLine("taban: " + Math.Ceiling(sayi6) ); // 98
+            Console.WriteLine("taban: " + Math.Ceiling(sayi6) ); // 99
+
+            Console.WriteLine("Yuvarlama: " + Math.Round(sayi5) ); // 12
+            Console.WriteLine("Yuvarlama: " + Math.Round(sayi6) ); // 99
+
+            Console.WriteLine("1 haneye göre yuvarlama: " + Math.Round(sayi5, 1) ); // 12.3
+            Console.WriteLine("1 haneye göre yuvarlama: " + Math.Round(sayi6, 1) ); // 98.8
+
+            Console.WriteLine("2 haneye göre yuvarlama: " + Math.Round(sayi5, 2) ); // 12.34
+            Console.WriteLine("2 haneye göre yuvarlama: " + Math.Round(sayi6, 2) ); // 98.76
+
+
+            double yariCap, sonuc;
+            string hesaplama, piGercekMi;
+
+            Console.Write("Yarıçap (0: çıkış)");
+            yariCap = Convert.ToDouble(Console.ReadLine(), new CultureInfo("tr-TR"));
+            while( yariCap != 0)
+            {
+                Console.Write("Hesaplama (a: alan, ç: çevre): ");
+                hesaplama = Console.ReadLine();
+                Console.Write("Pİ gerçek değeri mi? (e:evet, h:hayır): ");
+                piGercekMi = Console.ReadLine();
+                sonuc = Hesaplama(yariCap, hesaplama, piGercekMi);
+                Console.WriteLine((hesaplama == "a" ? "Alan" : "Çevre") + sonuc);
+            }
+
+            for(decimal i = 1.0m; i <= 1.9m; i += 0.1m)
+            {
+                if(i == 2.5m)
+                {
+                    Console.WriteLine("2.5 için yuvarlama: " + Math.Round(i) );
+                }
+                else
+                {
+                    Console.WriteLine("Yuvarlama: " + Math.Round(i) );
+                }
+            }
+
+            decimal dno1 = 1.1m;
+            decimal dno2 = dno1 + 0.1m; // 1,2
+            if(dno2 == 1.2m)
+            {
+                Console.WriteLine("eşit");
+            }
+            else
+            {
+                Console.WriteLine("eşit değil");
+            }
+       
+
+        }
+
+        static double Hesaplama(double yariCap, string hesaplama, string piGercekMi)
+        {
+            double sonuc;
+            double pi = piGercekMi == "e" ? Math.PI : 3.14;
+            if(hesaplama == "a")
+            {
+                sonuc = pi * Math.Pow(yariCap, 2);
+            }
+            else
+            {
+                sonuc = 2 * pi * yariCap;
+            }
+            return sonuc;
+        }
+    }
+}
